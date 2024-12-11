16122470754195f3afe19a216775d56be2ff902e
Babeloula <walid.benzina@yahoo.fr>
2024-12-11 02:08:45 +0100

    Revert "Responsiveness platformer"
    This reverts commit ce68e9e209e87b6a9ccf73aa61b5ea4e0b686a17.

--- src/lib/games/platformer/config.ts
+++ src/lib/games/platformer/config.ts
@@ -10,15 +10,6 @@ export const gameConfig: Types.Core.GameConfig = {
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 1600,
     height: 960,
-    min: {
-      width: 320,
-      height: 192,
-    },
-    max: {
-      width: 1600,
-      height: 960,
-    },
-    zoom: 1,
   },
   physics: {
     default: "arcade",
